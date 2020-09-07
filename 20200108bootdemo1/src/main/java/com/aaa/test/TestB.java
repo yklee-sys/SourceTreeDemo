97a0081f0696ccb7265a7006545b0dab12a11bdb
yklee-sys <ykleemail@163.com>
2020-09-07 16:20:34 +0800

    修改B并提交

---
+++ 20200108bootdemo1/src/main/java/com/aaa/test/TestB.java
@@ -0,0 +1,15 @@
+package com.aaa.test;
+
+/**
+ * @Param
+ * @ClassName TestA
+ * @Description TODO
+ * @Author yk
+ * @Date 2020/9/7 0007 15:57
+ * @Return
+ **/
+public class TestA {
+    public static void main(String[] args) {
+        System.out.println("B测试");
+    }
+}
